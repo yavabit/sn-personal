b92cbdf3ceb2b13ec32ec579b8a55fb4b07df96d
Anton Zhilenkov <a.zhilenkov@globexit.ru>
2025-02-21 19:07:49 +0300

    Updates

--- src/shared/types/audio.ts
+++
@@ -1,9 +0,0 @@
-export interface AudioObject {
-	id: number;
-	name: string;
-	artist: string;
-	album?: string;
-	year?: number;
-	image?: string;
-	src: string;
-}
--- src/shared/types/task.ts
+++
@@ -1,7 +0,0 @@
-export interface ITask {
-	id: number
-	title: string
-	cost: number
-	state: string
-	unreachable?: boolean
-}
